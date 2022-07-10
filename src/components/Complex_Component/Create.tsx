128a16370edce76b8382a965eb2a8328f4c7d2f3
LaithAlebrahim <96999016+LaithAlebrahim@users.noreply.github.com>
2022-07-10 15:40:53 +0300

    We added (react-hook-form)

--- src/components/Complex_Component/Create.tsx
+++ src/components/Complex_Component/Create.tsx
@@ -1,18 +1,26 @@
 /* eslint-disable react/button-has-type */
 /* eslint-disable react/self-closing-comp */
-import React, { useState } from "react";
-
+/* eslint-disable react/jsx-props-no-spreading */
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { addDoc, collection } from "firebase/firestore";
 import { db, auth } from "../firebase-config";
 import styles from "./Create.module.css";
 
 function Create() {
   const [title, setTitle] = useState("");
+
   const [Body, setBody] = useState("");
+
   const [isSubmmiting, setIsSubmmiting] = useState(false);
+
   const postCollectionRef = collection(db, "posts");
-  const creatpost = async () => {
-    if (title.length > 0 && Body.length > 0) {
+
+  const { register, handleSubmit,formState: { errors } } = useForm();
+
+  const onSubmit = async () => {
+    setIsSubmmiting(true);
+  
       setIsSubmmiting(true);
       await addDoc(postCollectionRef, {
         title,
@@ -23,44 +31,53 @@ function Create() {
       });
 
       window.location.pathname = "/blogproject4";
-    }
+    
   };
 
   return (
     <div className={styles.create}>
       <h2 className={styles.create__title}>Add New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className={styles.blog__title}>
           <input
             className={styles.blog__title__text}
             type="text"
             required
             value={title}
-            placeholder="Write here blog's title"
+            {...register("Title", { required: true })}
+            
             onChange={(e) => setTitle(e.target.value)}
+            placeholder="Wrtite your title here"
           />
+               {errors.Title && <span>This field is required</span>}
         </div>
         <div className={styles.blog__text}>
           <textarea
             className={styles.blog__text__text}
             required
             value={Body}
+            {...register("Body", { required: true })}
             placeholder="Write here blog's text"
             onChange={(e) => setBody(e.target.value)}
           ></textarea>
+            {errors.Body && <span>This field is required</span>}
         </div>
-      </form>
-      <div className={styles.blog__bottom}>
+        <div className={styles.blog__bottom}>
         {isSubmmiting ? (
           <button className={styles.blog__button} disabled>
             Submitting...
           </button>
         ) : (
-          <button className={styles.blog__button} onClick={creatpost}>
+          <button
+            className={styles.blog__button}
+            type="submit"
+          >
             Submit
           </button>
         )}
       </div>
+      </form>
+      
     </div>
   );
 }
